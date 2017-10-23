f18370a5cc0158993aab90d6f9c6ba987c5b2845
sagittaria <shuyang_x@hotmail.com>
2017-10-23 16:41:22 +0800

    pci -ch3-4 k-means聚类

--- book10_programmingCollectiveIntelligence/ch-3-clustering.py
+++ book10_programmingCollectiveIntelligence/ch-3-clustering.py
@@ -6,6 +6,7 @@
 
 from math import sqrt
 from PIL import Image,ImageDraw
+import random
 
 def readfile(filename):
     lines=[line for line in open(filename)]
@@ -86,7 +87,7 @@ def hcluster(rows,distance=pearson):
         clust.append(newcluster)#把新合成的聚类append上去
     return clust[0] #最终返回了树的根节点
 
-clust=hcluster(data)
+#clust=hcluster(data)
 
 def getheight(clust):
     #如果是叶节点，高度为1；否则，高度是所有分支的高度之和
@@ -138,4 +139,65 @@ def drawnode(draw,clust,x,y,scaling,labels):
         draw.text((x+5,y-6),labels[clust.id],(0,0,0))
 
 
-drawdendrogram(clust,blognames,jpeg='blogclust.jpg')
+#drawdendrogram(clust,blognames,jpeg='blogclust.jpg')
+
+'''有时候在单行内的列聚类也是有意义的，如通过分析消费行为，用来发现哪些商品可以捆绑销售
+在本章的博客例子中，列举类可以发现哪些单词会经常结合在一起使用'''
+def rotatematrix(data): 
+    newdata=[]
+    for i in range(len(data[0])):
+        newrow=[data[j][i] for j in range(len(data))]
+        newdata.append(newrow)
+    return newdata
+'''对原来的data转置一下，就能用上面写好的方法执行聚类'''
+#rdata=clusters.rotatematrix(data)
+#wordclust=clusters.hcluster(rdata)
+#clusters.drawdendrogram(wordclust,labels=words,jpeg='wordclust.jpg')
+'''也可能会找出一些反映使用模式的聚类，us/say/very/think这些词的出现说明博客风格偏主观'''
+
+
+'''k均值聚类'''
+def kcluster(rows,distance=pearson,k=4):
+    # 确定每列的最小和最大值
+    ranges=[(min([row[i] for row in rows]),max([row[i] for row in rows]))
+            for i in range(len(rows[0]))]
+                
+    # 随机生成k个中心点（每个点有 len(rows[0]) 列）,j只是个counter
+    clusters=[[random.random()*(ranges[i][1]-ranges[i][0])+ranges[i][0]
+        for i in range(len(rows[0]))] for j in range(k)]
+        
+    lastmatches=None
+    for t in range(100):
+        print('Iteration %d' % t)
+        bestmatches=[[] for i in range(k)] #先用k个空列表填到bestmatches
+        
+        for j in range(len(rows)):#遍历每一行，确定它最接近于哪个中心点
+            row=rows[j]
+            bestmatch=0 #初始化“当前最近的中心”为第0个随机点
+            for i in range(k):#看这行（这个“点”）和第几个中心点最接近
+                d=distance(clusters[i],row)
+                if d<distance(clusters[bestmatch],row):
+                    bestmatch=i #如果距离更短，更新“最近点”
+            bestmatches[bestmatch].append(j)#把第j行追加到代表第bestmatch个中心的列表里
+    
+        # 如果这次迭代结果和上次相同，说明聚类完成了
+        if bestmatches==lastmatches: break
+        lastmatches=bestmatches #否则更新“上一次”的聚类结果
+        
+        # 求均值更新聚类中心点
+        for i in range(k):
+            avgs=[0.0]*len(rows[0]) #将avgs初始化为包含“列数”个0的列表
+            if len(bestmatches[i])>0: #如果第i个聚类中有内容
+                for rowid in bestmatches[i]:#遍历这个聚类中的每行（每个博客）
+                    for m in range(len(rows[rowid])):
+                        avgs[m]+=rows[rowid][m]
+                    '''按行全加起来，除以行数'''                        
+                for j in range(len(avgs)):
+                    avgs[j]/=len(bestmatches[i])
+                    '''求得平均值，更新替换原来的第i个中心'''                
+                clusters[i]=avgs
+                
+    return bestmatches
+
+clust=kcluster(data)
+'''“初始化”过程中有随机因素，所以结果不很稳定'''
