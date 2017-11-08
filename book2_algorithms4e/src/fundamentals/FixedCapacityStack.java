358d5f99a0d421c245012576c77ef853e0c0ba59
sagittaria <shuyang_x@hotmail.com>
2017-11-08 16:59:21 +0800

    rehabilitation-13

---
+++ book2_algorithms4e/src/fundamentals/FixedCapacityStack.java
@@ -0,0 +1,28 @@
+package fundamentals;
+
+public class FixedCapacityStack<Item> {
+    private Item[] a; // stack entries
+    private int N; // size
+
+    @SuppressWarnings("unchecked")
+    public FixedCapacityStack(int cap) {
+        a = (Item[]) new Object[cap];
+    }
+
+    public boolean isEmpty() {
+        return N == 0;
+    }
+
+    public int size() {
+        return N;
+    }
+
+    public void push(Item item) {
+        a[N++] = item;
+    }
+
+    public Item pop() {
+        return a[--N];
+    }
+
+}
--- book2_algorithms4e/src/month/DailySigning.java
+++ book2_algorithms4e/src/month/DailySigning.java
@@ -1,7 +1,8 @@
 package month;
 
 public class DailySigning {
-    public static void main(String args[]){
+    public static void main(String args[]) {
         System.out.println("2017-12-27 Sec.1.1 checked!");
+        System.out.println("2017-12-28 Sec.1.3 checked! P121");
     }
 }
