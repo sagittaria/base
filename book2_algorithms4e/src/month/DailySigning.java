792e878a12b9f0050d6b6b8491564a6700dd4886
sagittaria <shuyang_x@hotmail.com>
2017-12-03 17:07:36 +0800

    algs4-ch3-searching-3.1.4

--- book2_algorithms4e/src/month/DailySigning.java
+++ book2_algorithms4e/src/month/DailySigning.java
@@ -9,5 +9,6 @@ public class DailySigning {
         System.out.println("2018-01-11 20:27 Sec.2.3.2 reached! P185");
         System.out.println("2018-01-13 14:00 Sec.2.4 reached! P195");
         System.out.println("2018-01-20 18:18 Sec.3 reached! P227");
+        System.out.println("2018-01-27 16:49 Sec.3.1.4 reached! P235");
     }
 }
---
+++ book2_algorithms4e/src/searching/ST.java
@@ -0,0 +1,73 @@
+package searching;
+
+//public class ST<Key,Value> {
+public class ST<Key extends Comparable<Key>, Value> {
+    private int size;
+
+    ST() {
+    }
+
+    void put(Key key, Value val) {
+    }
+
+    Value get(Key key) {
+    }
+
+    void delete(Key key) {
+        put(key, null);
+    }
+
+    boolean contains(Key key) {
+        return get(key) != null;
+    }
+
+    boolean isEmpty() {
+        return size == 0;
+    }
+
+    int size() {
+        return size;
+    }
+
+    Key min() {
+    }
+
+    Key max() {
+    }
+
+    Key floor(Key key) {
+    }
+
+    Key ceiling(Key key) {
+    }
+
+    int rank(Key key) {
+    }
+
+    Key select(int k) {
+    }
+
+    void deleteMin() {
+        delete(min());
+    }
+
+    void deleteMax() {
+        delete(max());
+    }
+
+    int size(Key lo, Key hi) {
+        if (hi.compareTo(lo) < 0)
+            return 0;
+        else if (contains(hi))
+            return rank(hi) - rank(lo) + 1;
+        else
+            return rank(hi) - rank(lo); // 这行算什么意思？？
+    }
+
+    Iterable<Key> keys() {
+        return keys(min(), max());
+    }
+
+    Iterable<Key> keys(Key lo, Key hi) {
+    }
+}
