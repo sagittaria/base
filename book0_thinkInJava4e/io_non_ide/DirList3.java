acb32459b232dc2b876c7552d1591f8ba46f65ae
sagittaria <shuyang_x@hotmail.com>
2017-12-21 16:52:30 +0800

    File I/O

---
+++ book0_thinkInJava4e/io_non_ide/DirList3.java
@@ -0,0 +1,36 @@
+import java.io.File;
+import java.io.FileFilter;
+import java.io.FilenameFilter;
+import java.util.Arrays;
+import java.util.regex.Pattern;
+
+public class DirList3 {
+    public static void main(String[] args) {
+        File path = new File(".");
+        String[] list;
+        if (args.length == 0) {
+            list = path.list();
+        } else {
+            list = path.list(new FilenameFilter(){
+                private Pattern pattern = Pattern.compile(args[0]);
+                public boolean accept(File dir, String name){
+                    return pattern.matcher(name).matches();
+                }
+            });
+        }
+        Arrays.sort(list, String.CASE_INSENSITIVE_ORDER);
+        for (String dirItem : list) {
+            System.out.println(dirItem);
+        }
+    }
+
+    public static FilenameFilter filter(String regex) {
+        return new FilenameFilter(){
+            private Pattern pattern = Pattern.compile(regex);
+            public boolean accept(File dir, String name){
+                return pattern.matcher(name).matches();
+            }
+        };
+    }
+
+}
